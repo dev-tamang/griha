e0a1c47d9dd9ed15a73837852f9ee118987c1c0a
dev tamang <dev.tamang@proton.me>
2024-02-15 18:56:06 +0100

    fix: final camunda variables #ISS2

--- reporter/src/main/java/ch/lt/home/helloworld/ProcessVariables.java
+++ reporter/src/main/java/ch/lt/home/helloworld/ProcessVariables.java
@@ -5,7 +5,7 @@ import ch.lt.home.helper.camunda.CamundaVariableDefinition;
 
 public class ProcessVariables {
 
-    public static CamundaVariableDefinition<Person> PERSON = new CamundaVariableDefinition<>("person", Person.class);
-    public static CamundaVariableDefinition<Person> PERSON2 = new CamundaVariableDefinition<>("person2", Person.class);
-    public static CamundaVariableDefinition<String> TESTER = new CamundaVariableDefinition<>("tester", String.class);
+    public static final CamundaVariableDefinition<Person> PERSON = new CamundaVariableDefinition<>("person", Person.class);
+    public static final CamundaVariableDefinition<Person> PERSON2 = new CamundaVariableDefinition<>("person2", Person.class);
+    public static final CamundaVariableDefinition<String> TESTER = new CamundaVariableDefinition<>("tester", String.class);
 }
